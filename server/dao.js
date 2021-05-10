e0c8fb3ccfdc68b25834cd4f87d65e49f6f7d121
JonnyMarsiano <jonnymarsiano@gmail.com>
2021-05-10 12:48:37 +0200

    Update and Delete functions in dao.js

--- server/dao.js
+++ server/dao.js
@@ -1,25 +1,54 @@
 'use strict';
 /* Data Access Object (DAO) module for accessing courses and exams */
 
-const sqlite = require('sqlite3');  
+const sqlite = require('sqlite3');
 
 // open the database
 const db = new sqlite.Database('tasks.db', (err) => {
-    if(err) throw err;
-  });
+  if (err) throw err;
+});
 
 
-  // get all courses
+// get all tasks
 exports.listTasks = () => {
-    return new Promise((resolve, reject) => {
-      const sql = 'SELECT * FROM tasks';
-      db.all(sql, [], (err, rows) => {
-        if (err) {
+  return new Promise((resolve, reject) => {
+    const sql = 'SELECT * FROM tasks';
+    db.all(sql, [], (err, rows) => {
+      if (err) {
+        reject(err);
+        return;
+      }
+      const courses = rows.map((t) => ({ id: t.id, description: t.description, important: t.important, private: t.private, deadline: t.deadline }));
+      resolve(courses);
+    });
+  });
+};
+
+//update a task
+exports.updateTask = (task) => {
+  return new Promise((resolve, reject) => {
+    const sql = "UPDATE tasks SET description=? important=? private=? deadline=? completed=? user=? WHERE id=?";
+    db.run(sql, [task.description, task.important, task.private, task.deadline, task.completed, task.user],
+      function(err) {
+        if(err){
           reject(err);
           return;
         }
-        const courses = rows.map((t) => ({ id: t.id, description: t.description, important: t.important, private : t.private, deadline : t.deadline }));
-        resolve(courses);
-      });
-    });
-  };
+        resolve(task.id);
+      })
+  });
+}
+
+//delete a task
+exports.deleteTask = (id) => {
+  return new Promise((resolve,reject) => {
+    sql = "DELETE FROM tasks WHERE id=?";
+    db.run(sql, [id], function (err) {
+      if(err){
+        reject(err);
+        return;
+      }
+      resolve(null);
+    })
+  });
+}
