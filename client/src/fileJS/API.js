4998bb6222383a0a3a2efb3656b7f614295167f1
JonnyMarsiano <jonnymarsiano@gmail.com>
2021-05-17 19:31:27 +0200

    no message

--- client/src/fileJS/API.js
+++ client/src/fileJS/API.js
@@ -1,16 +1,6 @@
 import { Task } from "../TaskListCreate";
 
 const fetchTasks = async () => {
-<<<<<<< HEAD
-    const response = await fetch('/api/tasks');
-    console.log("resp :"+ response);
-    const responseBody = await response.json();
-    console.log("resp body:"+ responseBody);
-    return responseBody;
-};
-
-export { fetchTasks };
-=======
   const response = await fetch("/api/tasks");
   const responseBody = await response.json();
   return responseBody;
@@ -23,4 +13,3 @@ const fetchAddTask = async (task) => {
   });
   return response.status;
 };
->>>>>>> 9c1385b6d2e5ee67c9b25710fb88ca6dd522b40d
