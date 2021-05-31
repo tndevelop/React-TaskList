6849f7bc0399c3add3d2a353d2b03a45d630ab97
Daniele R.C <darthreca@gmail.com>
2021-05-31 12:35:53 +0200

    LoginForm LogoutButton

--- client/src/App.js
+++ client/src/App.js
@@ -9,8 +9,9 @@ import "./components/TaskList.js";
 import { Task, List } from "./TaskListCreate";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { CentralRow } from "./components/CentralRow";
-import API from './fileJS/API.js';
-import dayjs from 'dayjs';
+import { LoginForm } from "./components/LoginForm";
+import API from "./fileJS/API.js";
+import dayjs from "dayjs";
 
 // create the task list and add the dummy tasks
 // id, description, urgent, private, deadline
@@ -25,22 +26,45 @@ function App() {
   useEffect(() => {
     const getTasks = async () => {
       const tasks = await API.fetchTasks(filter);
-      tasks.map(t => t.deadline = t.deadline ? dayjs(t.deadline) : ""); //deadline from string to dayjs
+      tasks.map((t) => (t.deadline = t.deadline ? dayjs(t.deadline) : "")); //deadline from string to dayjs
       DummyTaskList.reset();
-      tasks.forEach(t => DummyTaskList.createElementFromServer(t.id, t.description, t.important, t.private, t.deadline, t.completed, t.user));
+      tasks.forEach((t) =>
+        DummyTaskList.createElementFromServer(
+          t.id,
+          t.description,
+          t.important,
+          t.private,
+          t.deadline,
+          t.completed,
+          t.user
+        )
+      );
       setTaskList(DummyTaskList.getList());
-    }
-    if (dirty && filter!=="undef") {
+    };
+    if (dirty && filter !== "undef") {
       getTasks().then(() => {
         setLoading(false);
         setDirty(false);
-        
       });
     }
   }, [dirty, filter]);
 
-  const addElementAndRefresh = (description, isUrgent, isPrivate, deadline, isCompleted, status) => {
-    DummyTaskList.createElement(description, isUrgent, isPrivate, deadline, isCompleted, status);
+  const addElementAndRefresh = (
+    description,
+    isUrgent,
+    isPrivate,
+    deadline,
+    isCompleted,
+    status
+  ) => {
+    DummyTaskList.createElement(
+      description,
+      isUrgent,
+      isPrivate,
+      deadline,
+      isCompleted,
+      status
+    );
     setTaskList(DummyTaskList.getList());
     setAddedTask(!addedTask);
   };
@@ -52,89 +76,114 @@ function App() {
 
   const setDone = async (task, done) => {
     deleteLocal(task);
-    addElementAndRefresh(task.description, task.important, task.private, task.deadline, done, "warning");
+    addElementAndRefresh(
+      task.description,
+      task.important,
+      task.private,
+      task.deadline,
+      done,
+      "warning"
+    );
     task.setDone(done);
     await API.fetchMarkTask(task);
     setDirty(true);
-    
   };
 
   const removeTask = async (task) => {
     //DummyTaskList.remove(task);
     //setTaskList((taskList) => taskList.filter((t) => t.id !== task.id));
     deleteLocal(task);
-    addElementAndRefresh(task.description, task.important, task.private, task.deadline, task.completed, "danger");//add the task with the status alert
+    addElementAndRefresh(
+      task.description,
+      task.important,
+      task.private,
+      task.deadline,
+      task.completed,
+      "danger"
+    ); //add the task with the status alert
     await API.fetchDeleteTask(task);
     setDirty(true);
   };
 
   const markTask = (task) => {
     task.completed = !task.completed;
-    API.fetchMarkTask(new Task(task.id, task.description, task.important, task.private, task.deadline, task.completed, task.user));
+    API.fetchMarkTask(
+      new Task(
+        task.id,
+        task.description,
+        task.important,
+        task.private,
+        task.deadline,
+        task.completed,
+        task.user
+      )
+    );
     setDirty(true);
-  }
-  /**
-   * Apply filter to `taskList`
-   * @param {string} filterName
-   * @returns {Array<Task>} filtered list
-   */
+  };
+
   return (
     <Router>
       <Container fluid="true">
         <MyNavbar></MyNavbar>
         <Switch>
+          <Route exact path="/login" render={() => <LoginForm />}></Route>
           <Route
             exact
             path="/:selectedFilter"
             render={({ match }) => {
-              
-              const filters = ["All", "Private", "Important", "Next7Days", "Today"];
+              const filters = [
+                "All",
+                "Private",
+                "Important",
+                "Next7Days",
+                "Today",
+              ];
               if (!filters.includes(match.params.selectedFilter)) {
                 setFilter("All");
-              }
-              else {
+              } else {
                 setFilter(match.params.selectedFilter);
               }
               if (loading) {
-                return (<p id="loading">Please wait, loading your tasks...</p>);
+                return <p id="loading">Please wait, loading your tasks...</p>;
+              } else {
+                return (
+                  <CentralRow
+                    selectedFilter={match.params.selectedFilter}
+                    setFilter={setFilter}
+                    setDone={setDone}
+                    createElement={addElementAndRefresh}
+                    taskList={taskList}
+                    removeTask={removeTask}
+                    setDirty={setDirty}
+                    setLoading={setLoading}
+                    deleteLocal={deleteLocal}
+                  ></CentralRow>
+                );
               }
-              
-              else {
-                return (<CentralRow
-                  selectedFilter={match.params.selectedFilter}
-                  setFilter={setFilter}
-                  setDone={setDone}
-                  createElement={addElementAndRefresh}
-                  taskList={taskList}
-                  removeTask={removeTask}
-                  setDirty={setDirty}
-                  setLoading={setLoading}
-                  deleteLocal={deleteLocal}
-                ></CentralRow>);
-              }
-            }} />
+            }}
+          />
           <Route
             exact
             path="/"
             render={() => {
               if (loading) {
-                return (<p id="loading">Please wait, loading your tasks...</p>);
+                return <p id="loading">Please wait, loading your tasks...</p>;
+              } else {
+                return (
+                  <CentralRow
+                    selectedFilter={"All"}
+                    setFilter={setFilter}
+                    setDone={setDone}
+                    createElement={addElementAndRefresh}
+                    taskList={taskList}
+                    removeTask={removeTask}
+                    setDirty={setDirty}
+                    setLoading={setLoading}
+                    deleteLocal={deleteLocal}
+                  ></CentralRow>
+                );
               }
-              else {
-                return (<CentralRow
-                  selectedFilter={"All"}
-                  setFilter={setFilter}
-                  setDone={setDone}
-                  createElement={addElementAndRefresh}
-                  taskList={taskList}
-                  removeTask={removeTask}
-                  setDirty={setDirty}
-                  setLoading={setLoading}
-                  deleteLocal={deleteLocal}
-                ></CentralRow>);
-              }
-            }
-            }
+            }}
           />
         </Switch>
       </Container>
